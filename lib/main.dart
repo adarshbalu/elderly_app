87bfeee30bbdb123ee59ca80c16b17a0b8ed6203
Adarsh <adarshlp98@gmail.com>
2020-01-17 14:43:49 +0530

    Screens added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:elderly_app/screens/loading_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() => runApp(ElderlyApp());
 
@@ -7,6 +9,7 @@ class ElderlyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Elderly Care',
       home: LoadingScreen(),
     );
--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -10,32 +11,55 @@ class _LoadingScreenState extends State<LoadingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Flexible(
-              child: Container(
-                padding: EdgeInsets.all(90.0),
-                child: Image(
-                  image: AssetImage('images/logo.png'),
+      backgroundColor: Color(0xff00E676),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(40.0),
+              child: Image(
+                image: AssetImage('lib/resources/images/logo.png'),
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+            child: Text(
+              'Elderly Care',
+              style: TextStyle(
+                fontSize: 60.0,
+                fontWeight: FontWeight.w300,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 10.0,
+          ),
+          Expanded(
+            child: Container(
+              margin: EdgeInsets.all(20.0),
+              child: Text(
+                'We care',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w700,
+                  fontSize: 30.0,
                 ),
               ),
             ),
-            Text(
-              'Elderly Care',
-              style: TextStyle(
+          ),
+          Expanded(
+            child: Container(
+              child: SpinKitRipple(
                 color: Colors.white,
-                fontSize: 60.0,
-                fontWeight: FontWeight.w200,
+                size: 90.0,
               ),
             ),
-            CircularProgressIndicator(),
-          ],
-        ),
-        decoration: BoxDecoration(
-          color: Color(0xff00C853),
-        ),
+          ),
+        ],
       ),
     );
   }
