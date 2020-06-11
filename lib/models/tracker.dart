fe3930d42cbad8505d64bd7c32fbdf5644fbae58
Adarsh <adarshlp98@gmail.com>
2020-06-11 18:28:19 +0530

    Started Tracker models

---
+++ lib/models/tracker.dart
@@ -0,0 +1,17 @@
+class TrackerModel {
+  SleepTracker sleepTracker;
+  WeightTracker weightTracker;
+  HeightTracker heightTracker;
+  BloodPressureTracker bloodPressureTracker;
+  BloodSugarTracker bloodSugarTracker;
+}
+
+class SleepTracker {}
+
+class WeightTracker {}
+
+class HeightTracker {}
+
+class BloodSugarTracker {}
+
+class BloodPressureTracker {}
