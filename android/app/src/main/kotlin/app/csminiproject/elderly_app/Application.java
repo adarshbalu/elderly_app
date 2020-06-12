6ea59fc4b61fef60ec2a609c92110296c96e7740
Adarsh <adarshlp98@gmail.com>
2020-06-12 18:14:48 +0530

    Added Alarm Manager plugin for background operations

---
+++ android/app/src/main/kotlin/app/csminiproject/elderly_app/Application.java
@@ -0,0 +1,21 @@
+package app.csminiproject.elderly_app ;
+
+import io.flutter.app.FlutterApplication;
+import io.flutter.plugin.common.PluginRegistry;
+import io.flutter.plugin.common.PluginRegistry.PluginRegistrantCallback;
+import io.flutter.plugins.androidalarmmanager.AlarmService;
+import io.flutter.plugins.androidalarmmanager.AndroidAlarmManagerPlugin;
+
+public class Application extends FlutterApplication implements PluginRegistrantCallback {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        AlarmService.setPluginRegistrant(this);
+    }
+
+    @Override
+    public void registerWith(PluginRegistry registry) {
+        AndroidAlarmManagerPlugin.registerWith(
+                registry.registrarFor("io.flutter.plugins.androidalarmmanager.AndroidAlarmManagerPlugin"));
+    }
+}
