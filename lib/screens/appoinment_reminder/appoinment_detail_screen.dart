2b9c165f412fc0924f573ea38d9a30ec97c8c3d2
Adarsh <adarshlp98@gmail.com>
2020-06-21 00:08:43 +0530

    Updated appoinment_detail_screen

--- lib/screens/appoinment_reminder/appoinment_detail_screen.dart
+++ lib/screens/appoinment_reminder/appoinment_detail_screen.dart
@@ -1,13 +1,13 @@
 import 'package:elderly_app/models/appoinment.dart';
 import 'package:elderly_app/others/database_helper.dart';
 import 'package:elderly_app/screens/appoinment_reminder/appoinment_reminder_screen.dart';
-import 'package:elderly_app/screens/profile/profile_screen.dart';
 import 'package:elderly_app/widgets/app_default.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_material_pickers/flutter_material_pickers.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:intl/intl.dart';
+import 'package:sweet_alert_dialogs/sweet_alert_dialogs.dart';
 
 class AppoinmentDetail extends StatefulWidget {
   static const String id = 'Appoinment_Detail_Screen';
@@ -51,7 +51,7 @@ class _AppoinmentDetailState extends State<AppoinmentDetail> {
     doctorName = nameController.text = appoinment.name;
     place = placeController.text = appoinment.place;
     address = addressController.text = appoinment.address;
-    //date = DateTime.parse(appoinment.dateAndTime);
+    date = DateTime.parse(appoinment.dateAndTime);
     super.initState();
   }
 
@@ -69,162 +69,205 @@ class _AppoinmentDetailState extends State<AppoinmentDetail> {
     return Scaffold(
       drawer: AppDrawer(),
       appBar: ElderlyAppBar(),
-      body: ListView(
-        children: <Widget>[
-          SizedBox(
-            height: 0,
-          ),
-          Center(
-            child: Padding(
-              padding: EdgeInsets.all(10),
-              child: Text(
-                '$pageTitle Appoinment',
-                style: TextStyle(color: Colors.green, fontSize: 28),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 0,
-          ),
-          AppoinmentFormItem(
-            helperText: 'Full name',
-            hintText: 'Enter name of the Doctor',
-            controller: nameController,
-            onChanged: (value) {
-              setState(() {
-                doctorName = value;
-              });
-            },
-            isNumber: false,
-            icon: FontAwesomeIcons.userMd,
-          ),
-          AppoinmentFormItem(
-            helperText: 'Hospital , Home',
-            hintText: 'Enter place of Visit',
-            controller: placeController,
-            onChanged: (value) {
-              setState(() {
-                place = value;
-              });
-            },
-            isNumber: false,
-            icon: FontAwesomeIcons.clinicMedical,
-          ),
-          AppoinmentFormItem(
-            helperText: 'Any Specialization',
-            hintText: 'Enter type ',
-            controller: addressController,
-            onChanged: (value) {
-              setState(() {
-                address = value;
-              });
-            },
-            isNumber: false,
-            icon: FontAwesomeIcons.briefcaseMedical,
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
+      body: WillPopScope(
+        onWillPop: () async {
+          if (appoinment !=
+              Appoinment(doctorName, place, date.toString(), address)) {
+            return showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return RichAlertDialog(
+                    alertTitle: richTitle("Reminder Not Saved"),
+                    alertSubtitle: richSubtitle('Changes will be discarded '),
+                    alertType: RichAlertType.WARNING,
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("OK"),
+                        onPressed: () {
+                          Navigator.of(context).pushAndRemoveUntil(
+                              MaterialPageRoute(
+                                  builder: (context) => AppoinmentReminder()),
+                              (Route<dynamic> route) => false);
+                        },
+                      ),
+                      FlatButton(
+                        child: Text("No"),
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                      ),
+                    ],
+                  );
+                });
+          } else
+            return true;
+        },
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Date',
-                      style: TextStyle(color: Colors.teal),
-                    ),
-                    InkWell(
-                      child: CircleAvatar(
-                        radius: 40,
-                        backgroundColor: Colors.green,
-                        child: Icon(
-                          Icons.event_note,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onTap: () async {
-                        await _selectDate();
-                        print(tempDate.toString());
-                      },
-                    ),
-                  ],
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '$pageTitle Appoinment',
+                    style: TextStyle(color: Colors.green, fontSize: 28),
+                  ),
                 ),
               ),
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Text(
-                      'Time',
-                      style: TextStyle(color: Colors.teal),
-                    ),
-                    InkWell(
-                      child: CircleAvatar(
-                        radius: 40,
-                        backgroundColor: Colors.green,
-                        child: Icon(
-                          Icons.alarm_add,
-                          size: 40,
-                          color: Colors.white,
-                        ),
-                      ),
-                      onTap: () async {
-                        showMaterialTimePicker(
-                          context: context,
-                          selectedTime: timeSelected,
-                          onChanged: (value) => setState(() {
-                            timeSelected = value;
-                            print(timeSelected.toString());
-                          }),
-                        );
-                      },
-                    ),
-                  ],
-                ),
+              SizedBox(
+                height: 15,
               ),
+              AppoinmentFormItem(
+                helperText: 'Full name',
+                hintText: 'Enter name of the Doctor',
+                controller: nameController,
+                onChanged: (value) {
+                  setState(() {
+                    doctorName = value;
+                  });
+                },
+                isNumber: false,
+                icon: FontAwesomeIcons.userMd,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              AppoinmentFormItem(
+                helperText: 'Hospital , Home',
+                hintText: 'Enter place of Visit',
+                controller: placeController,
+                onChanged: (value) {
+                  setState(() {
+                    place = value;
+                  });
+                },
+                isNumber: false,
+                icon: FontAwesomeIcons.clinicMedical,
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              AppoinmentFormItem(
+                helperText: 'Any Specialization',
+                hintText: 'Enter type ',
+                controller: addressController,
+                onChanged: (value) {
+                  setState(() {
+                    address = value;
+                  });
+                },
+                isNumber: false,
+                icon: FontAwesomeIcons.briefcaseMedical,
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Date',
+                          style: TextStyle(color: Colors.teal),
+                        ),
+                        InkWell(
+                          child: CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.green,
+                            child: Icon(
+                              Icons.event_note,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onTap: () async {
+                            await _selectDate();
+                            print(tempDate.toString());
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          'Time',
+                          style: TextStyle(color: Colors.teal),
+                        ),
+                        InkWell(
+                          child: CircleAvatar(
+                            radius: 40,
+                            backgroundColor: Colors.green,
+                            child: Icon(
+                              Icons.alarm_add,
+                              size: 40,
+                              color: Colors.white,
+                            ),
+                          ),
+                          onTap: () async {
+                            showMaterialTimePicker(
+                              context: context,
+                              selectedTime: timeSelected,
+                              onChanged: (value) => setState(() {
+                                timeSelected = value;
+                                print(timeSelected.toString());
+                              }),
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 8,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(18.0, 16, 18, 1),
+                child: RaisedButton(
+                  elevation: 3,
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15)),
+                  hoverElevation: 5,
+                  highlightColor: Colors.green.withAlpha(10),
+                  color: Colors.green,
+                  padding: EdgeInsets.symmetric(vertical: 15, horizontal: 35),
+                  onPressed: () async {
+                    if (!(timeSelected.minute == 0 && timeSelected.hour == 0)) {
+                      if (!(tempDate.year == 0 &&
+                          tempDate.month == 0 &&
+                          tempDate.day == 0)) {
+                        setState(() {
+                          date = DateTime(
+                              tempDate.year,
+                              tempDate.month,
+                              tempDate.day,
+                              timeSelected.hour,
+                              timeSelected.minute);
+                        });
+
+                        _save();
+                        await Navigator.of(context).pushAndRemoveUntil(
+                            MaterialPageRoute(
+                                builder: (context) => AppoinmentReminder()),
+                            (Route<dynamic> route) => false);
+                      } else {}
+                    }
+                  },
+                  child: Text(
+                    '$pageTitle Appoinment',
+                    style: TextStyle(color: Colors.white, fontSize: 23),
+                  ),
+                ),
+              )
             ],
           ),
-          SizedBox(
-            height: 5,
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(18.0, 16, 18, 1),
-            child: MaterialButton(
-              elevation: 10,
-              hoverElevation: 5,
-              highlightColor: Colors.green.withAlpha(10),
-              color: Colors.green,
-              padding: EdgeInsets.all(15),
-              onPressed: () async {
-                if (!(timeSelected.minute == 0 && timeSelected.hour == 0)) {
-                  if (!(tempDate.year == 0 &&
-                      tempDate.month == 0 &&
-                      tempDate.day == 0)) {
-                    setState(() {
-                      date = DateTime(tempDate.year, tempDate.month,
-                          tempDate.day, timeSelected.hour, timeSelected.minute);
-                    });
-
-                    print(date.toString());
-                    _save();
-                    await Navigator.of(context).pushAndRemoveUntil(
-                        MaterialPageRoute(
-                            builder: (context) => AppoinmentReminder()),
-                        (Route<dynamic> route) => false);
-                  } else {
-                    print('fail');
-                  }
-                }
-              },
-              child: Text(
-                '$pageTitle Appoinment',
-                style: TextStyle(color: Colors.white, fontSize: 23),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
@@ -268,9 +311,9 @@ class _AppoinmentDetailState extends State<AppoinmentDetail> {
 class AppoinmentFormItem extends StatelessWidget {
   final String hintText;
   final String helperText;
-  Function onChanged;
+  final Function onChanged;
   final bool isNumber;
-  IconData icon;
+  final IconData icon;
   final controller;
 
   AppoinmentFormItem(
@@ -287,18 +330,17 @@ class AppoinmentFormItem extends StatelessWidget {
       child: TextField(
         decoration: InputDecoration(
           border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(15),
               borderSide: BorderSide(
                   color: Color(0xffaf5676), style: BorderStyle.solid)),
-          helperText: helperText,
-          icon: Icon(icon, color: Colors.green),
+          prefixIcon: Icon(icon, color: Colors.green),
           hintText: hintText,
           focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(15),
               borderSide:
                   BorderSide(color: Colors.indigo, style: BorderStyle.solid)),
           enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(30),
+              borderRadius: BorderRadius.circular(15),
               borderSide: BorderSide(
                   color: Color(0xffaf5676), style: BorderStyle.solid)),
         ),
